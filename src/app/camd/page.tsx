651b6e5b8922ff6d48982438e3bafed831526559
thisishuzaifa <huzaifa_khalid97@outlook.com>
2023-10-27 14:32:59 -0700

    added stuff, cleaned up UI, fixed mobile response

--- src/app/camd/page.tsx
+++ src/app/camd/page.tsx
@@ -1,12 +1,127 @@
 'use client';
+import { useState } from "react";
+import { motion } from "framer-motion";
 
-export default function CamdCheck(){
+type Question = {
+    question: string;
+    correctAnswer: string;
+    advice?: string;
+  };
+
+const questions: Question[] = [
+    {
+      question: "Are you currently enrolled in CAMD?",
+      correctAnswer: "Yes",
+      advice: "Must be enrolled in a CPS Program to be eligible for Co-Op.",
+    },
+    {
+      question: "Do you maintain a 3.0 CGPA as of this term?",
+      correctAnswer: "Yes",
+      advice: "Maintain a 3.0 CGPA to be eligible for co-op.",
+    },
+    {
+      question: "Have you completed 2 semesters?",
+      correctAnswer: "Yes",
+      advice: "Complete at least 2 semesters to be eligible for co-op.",
+    },
+    {
+      question: "Have you successfully completed ARTG 6000?",
+      correctAnswer: "Yes",
+      advice: "Successfully complete ARTG 6000 to be eligible for co-op.",
+    },
+    {
+      question: "Have you successfully completed ARTG 6040?",
+      correctAnswer: "Yes",
+      advice: "Successfully complete ARTG 6040 to be eligible for co-op.",
+    },
+  ];
+
+export default function CpsCheck(){
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [answers, setAnswers] = useState<Array<string>>([]);
+    const [eligibility, setEligibility] = useState<boolean | null>(null);
+  
+    const checkEligibility = (answer: string) => {
+      setAnswers([...answers, answer]);
+  
+      if (currentQuestion === questions.length - 1) {
+        const isEligible = answers.every(
+          (answer, index) => answer === questions[index]?.correctAnswer
+        );
+        setEligibility(isEligible);
+      } else {
+        setCurrentQuestion(currentQuestion + 1);
+      }
+    };
+  
+    const buttonVariants = {
+      hover: {
+        y: -5,
+        scale: 1.1,
+        transition: {
+          duration: 0.3,
+          ease: "easeInOut",
+        },
+      },
+    };
+
 
     return (
-        <h1 className="text-3xl font-bold mb-4 text-center">
-            Work in progress! Check back soon!
-        </h1>
-
-    );
+        <div className="min-h-screen bg-white flex flex-col justify-center items-center">
+      <div className="bg-white shadow-xl rounded-lg p-8">
+        {eligibility === null ? (
+          <div>
+            <h1 className="text-3xl font-bold mb-4 text-center">
+              {questions[currentQuestion]?.question}
+            </h1>
+            <div className="flex justify-center items-center space-x-4">
+              <motion.button
+                variants={buttonVariants}
+                whileHover="hover"
+                onClick={() => checkEligibility("Yes")}
+                className="m-2 p-2 bg-green-500 text-white w-24"
+              >
+                Yes
+              </motion.button>
+              <motion.button
+                variants={buttonVariants}
+                whileHover="hover"
+                onClick={() => checkEligibility("No")}
+                className="m-2 p-2 bg-red-500 text-white w-24"
+              >
+                No
+              </motion.button>
+            </div>
+          </div>
+        ) : (
+          <div>
+            {eligibility ? (
+              <h1 className="text-3xl font-bold mb-4 text-center">
+                You are eligible for co-op! 🎉
+              </h1>
+            ) : (
+              <div>
+                <h1 className="text-3xl font-bold mb-4 text-center">
+                  You are not eligible for co-op. 😢
+                </h1>
+                <ul className="list-disc pl-8">
+                  {questions.map((question, index) => {
+                    if (answers[index] !== question.correctAnswer) {
+                      return (
+                        <li key={index} className="mb-2">
+                          {question.advice}
+                        </li>
+                      );
+                    }
+                    return null;
+                  })}
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  );
     
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,11 @@
 'use client';
+import { useState } from "react";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
 const Navigation = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const navVariants = {
     hidden: {
       y: -50,
@@ -30,7 +33,7 @@ const Navigation = () => {
 
   return (
     <motion.nav
-      className="bg-white shadow-xl border-b border-gray-600"
+      className="bg-white shadow-lg mt-4 rounded-lg"
       variants={navVariants}
       initial="hidden"
       animate="visible"
@@ -39,9 +42,9 @@ const Navigation = () => {
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="hidden sm:-my-px sm:ml-6 sm:flex sm:space-x-8">
-              <Link href="/">
+            <Link href="/">
                 <motion.a
-                  className="inline-flex items-center px-1 pt-1 text-lg font-bold text-white-900 hover:text-gray-700"
+                  className="inline-flex items-center px-1 pt-1 text-lg font-serif font-bold text-gray-900 hover:text-gray-700"
                   variants={linkVariants}
                   whileHover="hover"
                 >
@@ -50,7 +53,7 @@ const Navigation = () => {
               </Link>
               <Link href="/courses">
                 <motion.a
-                  className="inline-flex items-center px-1 pt-1 text-lg font-bold text-white-900 hover:text-gray-700"
+                  className="inline-flex items-center px-1 pt-1 text-lg font-serif font-bold text-gray-900 hover:text-gray-700"
                   variants={linkVariants}
                   whileHover="hover"
                 >
@@ -59,7 +62,7 @@ const Navigation = () => {
               </Link>
               <Link href="/about">
                 <motion.a
-                  className="inline-flex items-center px-1 pt-1 text-lg font-bold text-white-900 hover:text-gray-700"
+                  className="inline-flex items-center px-1 pt-1 text-lg font-serif font-bold text-gray-900 hover:text-gray-700"
                   variants={linkVariants}
                   whileHover="hover"
                 >
@@ -68,6 +71,64 @@ const Navigation = () => {
               </Link>
             </div>
           </div>
+          <div className="-mr-2 flex items-center sm:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              type="button"
+              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+              aria-label="Main menu"
+              aria-expanded="false"
+            >
+              <svg
+                className={`${isOpen ? "hidden" : "block"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+              <svg
+                className={`${isOpen ? "block" : "hidden"} h-6 w-6`}
+                stroke="currentColor"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={`${isOpen ? "block" : "hidden"} sm:hidden`}>
+        <div className="px-2 pt-2 pb-3">
+          <Link href="/courses">
+            <motion.a
+              className="block px-3 py-2 rounded-md text-base font-bold text-gray-900 hover:text-gray-700"
+              variants={linkVariants}
+              whileHover="hover"
+            >
+              Courses
+            </motion.a>
+          </Link>
+          <Link href="/about">
+            <motion.a
+              className="block px-3 py-2 rounded-md text-base font-bold text-gray-900 hover:text-gray-700"
+              variants={linkVariants}
+              whileHover="hover"
+            >
+              About
+            </motion.a>
+          </Link>
         </div>
       </div>
     </motion.nav>
